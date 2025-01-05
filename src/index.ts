d796a254dec690f07328b170ec1d0781f6db6478
Mohit Sankhla <mohitsankhlabuzz@gmail.com>
2025-01-05 17:35:36 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import express, { Express } from "express";
+import { z } from "zod";
+import { prismaClient } from "./db";
+
+export const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const sumValidator = z.object({
+  a: z.number(),
+  b: z.number(),
+});
+
+app.post("/sum", async (req, res) => {
+  const { success, data, error } = await sumValidator.safeParseAsync(req.body);
+
+  if (!success) {
+    res.status(411).json({
+      error: error?.message,
+    });
+    return;
+  }
+
+  const { a, b } = data;
+  const sum = a + b;
+
+  await prismaClient.request.create({
+    data: {
+      a,
+      b,
+      result: sum,
+    },
+  });
+
+  res.status(200).json({
+    answer: sum,
+  });
+});
+
+app.post("/sum-with-id", async (req, res) => {
+  const { success, data, error } = await sumValidator.safeParseAsync(req.body);
+
+  if (!success) {
+    res.status(411).json({
+      error: error?.message,
+    });
+    return;
+  }
+
+  const { a, b } = data;
+  const sum = a + b;
+
+  const dbRequest = await prismaClient.request.create({
+    data: {
+      a,
+      b,
+      result: sum,
+    },
+  });
+
+  res.status(200).json({
+    answer: sum,
+    id: dbRequest.id,
+  });
+});
---
+++ src/tests/index.test.ts
@@ -0,0 +1,84 @@
+import { expect, it, describe, vi } from "vitest";
+import request from "supertest";
+import { app } from "..";
+import { prismaClient } from "../__mocks__/db";
+
+// SHALLOW MOCKING
+// vi.mock("../db", () => {
+//   return {
+//     prismaClient: {
+//       request: {
+//         create: vi.fn(),
+//       },
+//     },
+//   };
+// });
+
+// USING DEEP MOCKS OUT OF THE __mocks__ FOLDER
+vi.mock("../db");
+
+describe("GET /", async () => {
+  it("should return 200 and Hello World!", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World!");
+  });
+});
+
+describe("sum endpoint", async () => {
+  it("should return the sum of two numbers", async () => {
+    const res = await request(app).post("/sum").send({
+      a: 1,
+      b: 2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).post("/sum").send();
+    expect(res.statusCode).toBe(411);
+  });
+});
+
+describe("sum-with-id endpoint", async () => {
+  // The below test case returns an error because we are mocking the prisma client but it's expecting a reponse from the prisma client to work as we wish
+  //   it("should return the sum of two numbers and an id", async () => {
+  //     const res = await request(app).post("/sum-with-id").send({
+  //       a: 1,
+  //       b: 2,
+  //     });
+  //     expect(res.statusCode).toBe(200);
+  //     expect(res.body.answer).toBe(3);
+  //     expect(res.body.id).toBeDefined();
+  //   });
+
+  it("should return the sum of two numbers and an id", async () => {
+    // here we'll add a resolved value before calling the script
+
+    prismaClient.request.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 2,
+      result: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const res = await request(app).post("/sum-with-id").send({
+      a: 1,
+      b: 2,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+    expect(res.body.id).toBeDefined();
+    expect(res.body.id).toBe(1);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).post("/sum-with-id").send();
+    expect(res.statusCode).toBe(411);
+  });
+});
